eb8c558856bb2d8c19dcacdc546f664af346f639
Supred <supred5022@gmail.com>
2025-11-15 19:56:35 +0200

    Button test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { auth, googleProvider } from "./firebase/firebase";
+import { signInWithPopup } from "firebase/auth";
 
 function App() {
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log(result.user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Zanes Pušķīši</h1>
+      <button
+        onClick={loginWithGoogle}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Sign in with Google
+      </button>
     </div>
   );
 }
